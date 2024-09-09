ad79ba238f43cd33ef571eb42fc6607e0e9cef18
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-09-09 02:48:44 -0300

    reorganize files and directories and modify calendar to start on monday instead of sunday

--- index.php
+++ index.php
@@ -1,65 +1,19 @@
+<?php
+require 'includes/calendar.php';
+require 'includes/greetings.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Calendário Simples</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-  <?php
-    $date = new DateTime('now', new DateTimeZone('America/Bahia'));
-
-    function getGreeting($date) {
-      $hour = $date->format('H');
-
-      if($hour >= 4 && $hour < 12) { return "Bom dia!"; }
-      if($hour >= 12 && $hour < 18) { return "Boa tarde!"; }
-
-      return "Boa noite!";
-    }
-
-    function addLine($week, $currentDay) {
-      echo "<tr class='border'>";
-
-      foreach ($week as $index => $day) {
-        $class = '';
-        $dayNumber = $day ?? '';
-
-        if ($index == 0) {
-          $class = 'red';
-        } elseif ($index == 6) {
-          $class = 'bold';
-        }
-        
-        if ($dayNumber == $currentDay) {
-          $class .= ($class ? ' ' : '') . 'bold';
-        }
-
-        echo "<td class='border $class'>$dayNumber</td>";
-      }
-
-      echo "</tr>";
-    }
-
-    function fillCalendar($currentDay) {
-      $week = [];
-
-      for($day = 1; $day <= 31; $day++) {
-        $week[] = $day;
-
-        if(count($week) == 7) {
-          addLine($week, $currentDay);
-          $week = [];
-        }
-      }
-
-      if(!empty($week)) { addLine($week, $currentDay); }
-    }
-  ?>
-
   <header>
-    <h1><?= htmlspecialchars(getGreeting($date)); ?></h1>
+    <h1><?= htmlspecialchars(getGreetings($date)); ?></h1>
     <h2>Estamos em <?= $date->format('Y') ?></h2>
     <p>Agora são <?= $date->format('H') ?> horas e <?= $date->format('i') ?> minutos.</p>
   </header>
@@ -67,17 +21,17 @@
     <table class="border">
       <thead>
         <tr>
-          <th>Dom</th>
           <th>Seg</th>
           <th>Ter</th>
           <th>Qua</th>
           <th>Qui</th>
           <th>Sex</th>
           <th>Sáb</th>
+          <th>Dom</th>
         </tr>
       </thead>
       <tbody>
-        <?php fillCalendar($date->format('j')) ?>
+        <?php fillCalendar($date->format('j'), $firstDayOfMonth, $daysInMonth) ?>
       </tbody>
     </table>
   </main>
